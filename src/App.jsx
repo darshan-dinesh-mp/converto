085aa847071da89420bb97c016d3c78d6330a413
Darshan Dinesh <darshandineshmp@gmail.com>
2024-07-26 18:48:25 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <div className={`${a.appmain}`}>
         <Nav />
         <Options />
-        <div className='absolute bottom-0 text-red-400 z-10 bg-black rounded-md p-1'>
+        <div className='absolute bottom-0 text-red-400 z-10 bg-black rounded-md py-1 px-2'>
           <h1>Please bring it to my notice if there is any error or suggestion. <a href="https://github.com/darshan-dinesh-mp" className='underline underline-offset-2'>Connect me</a></h1>
         </div>
       </div>
--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -4,8 +4,9 @@ import n from "./Nav.module.css"
 function Nav() {
   return (
     <div>
-        <nav className={`${n.nav} absolute text-white font-bold`}>
+        <nav className={`${n.nav} absolute text-white font-bold flex items-center justify-between`}>
           <h3>CONVERTO</h3>
+          <a href="https://drive.usercontent.google.com/download?id=1uC4LNjfzocQlY9TFnI7qPKYMHV24dB45&export=download&authuser=0" target='_blank' className='font-medium text-xl px-2 py-1 md:px-4 md:py-2 border-2 rounded-xl duration-200 hover:bg-white hover:text-black' title='You can always download the latest version from here'>Download App</a>
         </nav>
     </div>
   )
